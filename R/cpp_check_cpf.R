9ead63313317149a8a1af6ad0816312d60232073
RCCSilva <rafael.coelho.x@gmail.com>
2018-11-26 16:25:11 -0200

    cpp_check mais fáceis de utilizar

--- R/cpp_check_cpf.R
+++ R/cpp_check_cpf.R
@@ -4,29 +4,31 @@
 #'
 #' @export
 
-cpp_check_cpf <- function(data, cpfs){
+cpp_check_cpf <- function(cpfs){
 
   if(!is.character(cpfs)){
     stop("cpfs must be character vector.")
   }
 
+  if(!all(nchar(cpfs) == 11)){
+    stop("cpfs must have 11 characters. Use cpp_cpf to adjust that.")
+  }
+
   ## Split
   split <- stringr::str_split_fixed(cpfs, "", 11)
-  split <- apply(split, 2, function(x) as.numeric(x))
+  split <- matrix(as.numeric(split), ncol = 11)
 
   ## First digit
-  weights <- seq(10, 2)
-  fd <- sweep(split[,1:9], MARGIN = 2, weights, `*`)
-  sum <- rowSums(fd)
+  weights <- matrix(seq(10, 2), ncol = 1)
+  sum <- as.numeric(split[,1:9] %*% weights)
   remainder <- sum %% 11
   first_digit <- 11 - remainder
   first_digit <- ifelse(first_digit > 9, 0, first_digit)
 
   ## Second digit
   split_fd <- cbind(split, first_digit, deparse.level = 0)
-  weights <- c(11, weights)
-  sd <- sweep(split_fd[,c(1:9, 12)], MARGIN = 2, weights, `*`)
-  sums <- rowSums(sd)
+  weights <- matrix(c(11, weights), ncol = 1)
+  sums <- as.numeric(split_fd[,c(1:9, 12)] %*% weights)
   remainders <- sums %% 11
   second_digit <- 11 - remainders
   second_digit <- ifelse(second_digit > 9, 0, second_digit)
@@ -35,9 +37,9 @@ cpp_check_cpf <- function(data, cpfs){
   cod_validate <- cbind(first_digit, second_digit, deparse.level = 0)
   cod_disponivel <-  split[,10:11]
   valid_code <- ifelse(cod_validate == cod_disponivel, 1, 0)
-  colnames(valid_code) <- c("first_digit_valid", "second_digit_valid")
-  valid_code <- tibble::as_tibble(valid_code)
 
-  return(tibble::as_tibble(cbind(data, valid_code)))
+  cpf_is_valid <- (valid_code[,1] == 1) & (valid_code[,2] == 1)
+
+  return(cpf_is_valid)
 }
 
--- R/cpp_check_titulo.R
+++ R/cpp_check_titulo.R
@@ -3,53 +3,50 @@
 #'
 #' @export
 
-cpp_check_titulos <- function(data, titulos, uf = NULL){
-
-  if(nrow(data) != length(titulos)){
-    stop("data and titulos must have the same length.")
-  }
-
+cpp_check_titulos <- function(titulos){
   ## Check 12 digits
-  teste_digitos <- ifelse(nchar(titulos) == 12, 1, 0)
+  if(any(nchar(titulos) != 12)){
+    stop("titulos must have 12 characters. Use cpp_titulos to adjust that.")
+  }
 
   ## Check UF
-  if(!is.null(uf)){
-    if(length(uf) != length(titulos)){
-      stop("uf and titulos must have the same length.")
-    }
-
-    teste_uf <- dplyr::case_when(uf == "SP" & substr(titulos, 9, 10) == "01" ~ 1,
-                                 uf == "MG" & substr(titulos, 9, 10) == "02" ~ 1,
-                                 uf == "RJ" & substr(titulos, 9, 10) == "03" ~ 1,
-                                 uf == "RS" & substr(titulos, 9, 10) == "04" ~ 1,
-                                 uf == "BA" & substr(titulos, 9, 10) == "05" ~ 1,
-                                 uf == "PR" & substr(titulos, 9, 10) == "06" ~ 1,
-                                 uf == "CE" & substr(titulos, 9, 10) == "07" ~ 1,
-                                 uf == "PE" & substr(titulos, 9, 10) == "08" ~ 1,
-                                 uf == "SC" & substr(titulos, 9, 10) == "09" ~ 1,
-                                 uf == "GO" & substr(titulos, 9, 10) == "10" ~ 1,
-                                 uf == "MA" & substr(titulos, 9, 10) == "11" ~ 1,
-                                 uf == "PB" & substr(titulos, 9, 10) == "12" ~ 1,
-                                 uf == "PA" & substr(titulos, 9, 10) == "13" ~ 1,
-                                 uf == "ES" & substr(titulos, 9, 10) == "14" ~ 1,
-                                 uf == "PI" & substr(titulos, 9, 10) == "15" ~ 1,
-                                 uf == "RN" & substr(titulos, 9, 10) == "16" ~ 1,
-                                 uf == "AL" & substr(titulos, 9, 10) == "17" ~ 1,
-                                 uf == "MT" & substr(titulos, 9, 10) == "18" ~ 1,
-                                 uf == "MS" & substr(titulos, 9, 10) == "19" ~ 1,
-                                 uf == "DF" & substr(titulos, 9, 10) == "20" ~ 1,
-                                 uf == "SE" & substr(titulos, 9, 10) == "21" ~ 1,
-                                 uf == "AM" & substr(titulos, 9, 10) == "22" ~ 1,
-                                 uf == "RO" & substr(titulos, 9, 10) == "23" ~ 1,
-                                 uf == "AC" & substr(titulos, 9, 10) == "24" ~ 1,
-                                 uf == "AP" & substr(titulos, 9, 10) == "25" ~ 1,
-                                 uf == "RR" & substr(titulos, 9, 10) == "26" ~ 1,
-                                 uf == "TO" & substr(titulos, 9, 10) == "27" ~ 1,
-                                 uf == "ZZ" & substr(titulos, 9, 10) == "28" ~ 1,
-                                 T                                           ~ 0)
-  } else {
-    teste_uf <- NA
-  }
+  # if(!is.null(uf)){
+  #   if(length(uf) != length(titulos)){
+  #     stop("uf and titulos must have the same length.")
+  #   }
+  #
+  #   teste_uf <- dplyr::case_when(uf == "SP" & substr(titulos, 9, 10) == "01" ~ 1,
+  #                                uf == "MG" & substr(titulos, 9, 10) == "02" ~ 1,
+  #                                uf == "RJ" & substr(titulos, 9, 10) == "03" ~ 1,
+  #                                uf == "RS" & substr(titulos, 9, 10) == "04" ~ 1,
+  #                                uf == "BA" & substr(titulos, 9, 10) == "05" ~ 1,
+  #                                uf == "PR" & substr(titulos, 9, 10) == "06" ~ 1,
+  #                                uf == "CE" & substr(titulos, 9, 10) == "07" ~ 1,
+  #                                uf == "PE" & substr(titulos, 9, 10) == "08" ~ 1,
+  #                                uf == "SC" & substr(titulos, 9, 10) == "09" ~ 1,
+  #                                uf == "GO" & substr(titulos, 9, 10) == "10" ~ 1,
+  #                                uf == "MA" & substr(titulos, 9, 10) == "11" ~ 1,
+  #                                uf == "PB" & substr(titulos, 9, 10) == "12" ~ 1,
+  #                                uf == "PA" & substr(titulos, 9, 10) == "13" ~ 1,
+  #                                uf == "ES" & substr(titulos, 9, 10) == "14" ~ 1,
+  #                                uf == "PI" & substr(titulos, 9, 10) == "15" ~ 1,
+  #                                uf == "RN" & substr(titulos, 9, 10) == "16" ~ 1,
+  #                                uf == "AL" & substr(titulos, 9, 10) == "17" ~ 1,
+  #                                uf == "MT" & substr(titulos, 9, 10) == "18" ~ 1,
+  #                                uf == "MS" & substr(titulos, 9, 10) == "19" ~ 1,
+  #                                uf == "DF" & substr(titulos, 9, 10) == "20" ~ 1,
+  #                                uf == "SE" & substr(titulos, 9, 10) == "21" ~ 1,
+  #                                uf == "AM" & substr(titulos, 9, 10) == "22" ~ 1,
+  #                                uf == "RO" & substr(titulos, 9, 10) == "23" ~ 1,
+  #                                uf == "AC" & substr(titulos, 9, 10) == "24" ~ 1,
+  #                                uf == "AP" & substr(titulos, 9, 10) == "25" ~ 1,
+  #                                uf == "RR" & substr(titulos, 9, 10) == "26" ~ 1,
+  #                                uf == "TO" & substr(titulos, 9, 10) == "27" ~ 1,
+  #                                uf == "ZZ" & substr(titulos, 9, 10) == "28" ~ 1,
+  #                                T                                           ~ 0)
+  # } else {
+  #   teste_uf <- NA
+  # }
 
   # Digito verificador
   x1 <- 2 * as.numeric(substr(titulos, 1, 1))
@@ -75,7 +72,7 @@ cpp_check_titulos <- function(data, titulos, uf = NULL){
 
   verif <- paste0(v1, v2)
 
-  teste_verif <- ifelse(verif == substr(titulos, 11, 12), 1, 0)
+  teste_verif <- ifelse(verif == substr(titulos, 11, 12), TRUE, FALSE)
 
-  tibble::as_tibble(cbind(data, teste_digitos, teste_uf, teste_verif))
+  return(teste_verif)
 }
