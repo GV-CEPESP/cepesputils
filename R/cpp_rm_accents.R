aaafea0586fb92f74f69fbd5387414375252c4c1
Gabriela Campos <37840407+Gabiscampos@users.noreply.github.com>
2019-04-08 15:14:08 -0300

    Add encoding option
    Makes possible to change original encoding - in case you are using "UTF-8"

--- R/cpp_rm_accents.R
+++ R/cpp_rm_accents.R
@@ -2,13 +2,13 @@
 #'
 #' @export
 
-cpp_rm_accents <- function(x)
+cpp_rm_accents <- function(x, encoding = "latin1")
 {
   if (.Platform$OS.type == "unix") {
     stringr::str_replace_all(iconv(x, to = "ASCII//TRANSLIT"),
                              "[`'\"^~]", "")
   }
   else {
-    gsub("`", "", iconv(x, from = "latin1", to = "ASCII//TRANSLIT"))
+    gsub("`", "", iconv(x, from = encoding, to = "ASCII//TRANSLIT"))
   }
 }
